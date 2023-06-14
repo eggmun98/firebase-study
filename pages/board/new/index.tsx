5e211a54c021d74fd5b4eaab84407cd667d2a552
eggmun98 <eggmun98@gmail.com>
2023-06-14 16:03:07 +0900

    [2023.06.14] Feat 게시글 작성 기능 구현

---
+++ pages/board/new/index.tsx
@@ -0,0 +1,5 @@
+import BoardWriteUI from "../../../src/components/board/write";
+
+export default function BoardWritePage() {
+  return <BoardWriteUI></BoardWriteUI>;
+}
---
+++ src/components/board/write/index.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from "react";
+import * as S from "./styleds";
+import { collection, getFirestore, addDoc } from "firebase/firestore/lite";
+import { app } from "../../../commons/firebase";
+
+export default function BoardWriteUI() {
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const onClickSubmit = async () => {
+    const board = collection(getFirestore(app), "board");
+    // collection은 특정 컬렉션에 대한 참조를 생성하는 역할을 한다.
+    // 그리고 getFirestore 함수를 통해 파이어베이스 앱 인스턴스 객체를 가져온다
+    // 즉 apa 변수는 initializeApp(firebaseConfig) 이 함수를 통해 파이어베이스를 초기화하여
+    // 파이어베이스 프로젝트 구성 정보가 담겨져 있다.
+    // 즉 그정보를 app 변수에 담겨져 있고 getFirestore은 새로운 인스턴스 객체를 생성해서 가져오는 역활
+    await addDoc(board, { writer, title, contents }); //
+    // getDoc는 컬렉션에 새로운 문서를 추가하는 작업을 수행한다.
+    // 왼쪽 매개변수 자리에는 참조할 컬렉션을 말하는 거다.
+    // 오른쪽은 추가할 문서의 데이터를 담는다.
+    alert("게시글 등록에 성공하였습니다.");
+  };
+
+  const onChangeWrite = (e: ChangeEvent<HTMLInputElement>) => {
+    setWriter(e.target.value); // target은 현재 이벤트 발생의 위치에서 자식의 위치를 알려줌
+  };
+
+  const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeContents = (e: ChangeEvent<HTMLInputElement>) => {
+    setContents(e.target.value);
+  };
+
+  return (
+    <S.Wrapper>
+      <div>
+        작성자: <input onChange={onChangeWrite}></input>
+      </div>
+      <div>
+        제목: <input onChange={onChangeTitle}></input>
+      </div>
+      <div>
+        내용: <input onChange={onChangeContents}></input>
+      </div>
+      <button onClick={onClickSubmit}>등록하기</button>
+    </S.Wrapper>
+  );
+}
